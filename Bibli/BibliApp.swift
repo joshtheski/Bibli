a35f1e4ed885760405363bb9e0c5384b571679b2
Joshua Gajewski <gajewski.j@icloud.com>
2021-06-30 14:02:27 -0400

    Initial Commit

---
+++ Bibli/BibliApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibliApp.swift
+//  Bibli
+//
+//  Created by Joshua on 2021-06-30.
+//
+
+import SwiftUI
+
+@main
+struct BibliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bibli/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bibli
+//
+//  Created by Joshua on 2021-06-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
